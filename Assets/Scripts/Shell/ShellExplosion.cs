a4b9e759e99ca11b986b08361f6afbbb42babece
Donghoon Im <dongttang@gmail.com>
2017-08-22 22:05:12 +0900

    tanks

---
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;
+    public ParticleSystem m_ExplosionParticles;       
+    public AudioSource m_ExplosionAudio;              
+    public float m_MaxDamage = 100f;                  
+    public float m_ExplosionForce = 1000f;            
+    public float m_MaxLifeTime = 2f;                  
+    public float m_ExplosionRadius = 5f;              
+
+
+    private void Start()
+    {
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+		Collider[] touchedColliders = Physics.OverlapSphere (this.transform.position, m_ExplosionRadius, m_TankMask);
+
+		for (int i = 0; i < touchedColliders.Length; i++) {
+
+			Rigidbody targetRigidbody = touchedColliders [i].GetComponent <Rigidbody> ();
+
+			if (targetRigidbody == null)
+				continue;
+
+			targetRigidbody.AddExplosionForce (m_ExplosionForce, this.transform.position, m_ExplosionRadius);
+
+			TankHealth targetHealth = targetRigidbody.GetComponent <TankHealth> ();
+
+			if (targetHealth == null)
+				continue;
+
+
+			float damage = CalculateDamage (targetRigidbody.GetComponent <Transform>().position);
+
+			targetHealth.TakeDamage (damage);
+
+		}
+
+		m_ExplosionParticles.transform.parent = null;
+
+		m_ExplosionParticles.Play ();
+
+		m_ExplosionAudio.Play ();
+
+		Destroy (m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+
+		Destroy (this.gameObject);
+
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+
+//		return m_MaxDamage / Vector3.Distance (targetPosition, this.transform.position);
+
+		Vector3 explosionToTarget = targetPosition - transform.position;
+
+		float explosionDistance = explosionToTarget.magnitude;
+
+		float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+
+		float damage = relativeDistance * m_MaxDamage;
+
+		damage = Mathf.Max (0f, damage);
+
+		return damage;
+
+    }
+}
+
+///*
+// * With the Shell GameObject still selected drag the child GameObject called ShellExplosion onto the ExplosionParticles and Explosion Audio public variables.
+// * set the Tank Mask public variable to Players.
+// * Drag the Shell GameObject to the Prefabs folder in the Project panel to save it as a prefab.
+// * 
+// * 
+//* Shell GameObject를 선택하고 ShellExplosion이라는 하위 GameObject를 ExplosionParticles 및 Explosion Audio 공용 변수로 드래그합니다.
+//  * Tank Mask 공용 변수를 Players로 설정하십시오.
+//  * 쉘 게임 객체를 [프로젝트] 패널의 [프리 팹] 폴더로 드래그하여 프리 팹으로 저장합니다.
+//  *
+//  *
+// * /
---
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankHealth : MonoBehaviour
+{
+    public float m_StartingHealth = 100f;
+    public Image m_FillImage;
+    public Color m_FullHealthColor = Color.green;
+    public Color m_ZeroHealthColor = Color.red;
+    public GameObject m_ExplosionPrefab;
+    
+	public float m_CurrentHealth;
+
+	private Slider m_Slider;
+    private AudioSource m_ExplosionAudio;
+    private ParticleSystem m_ExplosionParticles;
+    private bool m_Dead;
+
+
+    private void Awake()
+    {
+        m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
+
+        m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
+
+        m_ExplosionParticles.gameObject.SetActive(false);
+
+		m_Slider = GetComponentInChildren<Slider> ();
+
+    }
+
+
+    private void OnEnable()
+    {
+        m_CurrentHealth = m_StartingHealth;
+        m_Dead = false;
+
+        SetHealthUI();
+    }
+
+    public void TakeDamage(float amount)
+    {
+		m_CurrentHealth -= amount;
+
+		SetHealthUI ();
+
+		if (m_CurrentHealth <= 0) {
+
+			OnDeath ();
+
+		}
+    }
+
+
+    private void SetHealthUI()
+    {
+
+		m_Slider.value = m_CurrentHealth;
+//		if(m_CurrentHealth <= 0) {
+//
+//			// 색깔 설정
+//
+//		}
+        // Adjust the value and colour of the slider.
+    }
+
+
+    private void OnDeath()
+    {
+
+        // Play the effects for the death of the tank and deactivate it.
+
+    }
+
+}
